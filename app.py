79ca93caa806615714585c9cdba29b0dea0675ed
Lemkot <113555724+Lemkot@users.noreply.github.com>
2023-09-12 17:41:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ if (SP500_futures):
     API_url = "http://model1209-lemishkotetiana.b4a.run/"
     json_url = get_response(API_url)
     #st.write("## Json {}".format(json_url))
-    API_data = json_url.json() 
+    API_data = json_url 
     st.write(API_data["forecasted_SP_index"])
     
 #-------------------------------------------------------
